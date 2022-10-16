97e2f53b7270c54c1d4897e6f2223b2cdd8edb2f
Felix Wallin <nils.felix@gmail.com>
2022-10-16 20:38:36 +0200

    Add cycleCheckbox base

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,7 @@
-import { Plugin } from "obsidian";
+import { Plugin, Editor } from "obsidian";
 import { CheckboxCyclerPluginSettings, DEFAULT_SETTINGS, CheckboxCyclerSettingTab } from "./settings"
+
+const states = [" ", "/", "x", "-"];
 
 export default class CheckboxCyclerPlugin extends Plugin {
   settings: CheckboxCyclerPluginSettings;
@@ -8,11 +10,9 @@ export default class CheckboxCyclerPlugin extends Plugin {
     console.log("loading plugin");
 
     await this.loadSettings();
-		this.loadCommands();
-    this.loadRibbon();
+    this.loadCommands();
 
     this.addSettingTab(new CheckboxCyclerSettingTab(this.app, this));
-
   }
 
   onunload() {
@@ -27,9 +27,46 @@ export default class CheckboxCyclerPlugin extends Plugin {
     await this.saveData(this.settings);
   }
 
-	loadCommands() {
+  loadCommands() {
+    this.addCommand({
+      id: "cycle-checkbox",
+      name: "Cycle checkbox",
+      icon: "checkbox-glyph",
+      editorCallback: (editor: Editor) => this.cycleCheckbox(editor)
+    });
   }
 
-  loadRibbon() {
+  cycleCheckbox(editor: Editor) {
+    // - [ ]
+    let lineIndex = editor.getCursor().line;
+    let line = editor.getLine(lineIndex);
+    let lineTrimmed = line.trimStart();
+    let checkbox = lineTrimmed.substring(0, 5);
+
+    if (checkbox.length < 1) {
+      console.log("line too short");
+      return;
+    }
+    else if (!["-", "*", "+"].includes(checkbox[0])) {
+      console.log("line not list item");
+      return;
+    }
+    else if (checkbox[1] !== " " || checkbox[2] !== "[" || checkbox[4] !== "]") {
+      console.log("line not checkbox");
+      return;
+    }
+
+    let state = checkbox[3];
+    let stateIndex = line.length - lineTrimmed.length + 3;
+    let newState = states[(states.indexOf(state) + 1) % states.length];
+
+    console.log(checkbox);
+    console.log("line: " + lineIndex + ", ch: " + stateIndex);
+
+    editor.replaceRange(
+      newState,
+      {line: lineIndex, ch: stateIndex},
+      {line: lineIndex, ch: stateIndex + 1}
+    )
   }
 }
